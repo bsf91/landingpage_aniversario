517b9571b8a0aa2c72e7f40cfcb58a5198aa8728
bsf91 <brunoscartezinifukuda@gmail.com>
2025-06-30 19:09:07 -0300

    Estruturando seções e corrigindo responsividade landscape mobile

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+const dataDoEvento = new Date('2026-02-17T08:47:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML =
+        `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.querySelector('.hero__text').innerHTML = `O EVENTO JÁ COMCEÇOU`;
+        return;
+    }
+}, 1000);
